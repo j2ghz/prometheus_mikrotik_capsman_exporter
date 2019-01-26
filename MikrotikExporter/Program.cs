ac22fec0f6985df32a14a134f2fb077ca47b0719
J2ghz <j2.00ghz@gmail.com>
2019-01-26 16:58:44 +0100

    cleanup

--- MikrotikExporter/Program.cs
+++ MikrotikExporter/Program.cs
@@ -1,30 +1,32 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Prometheus;
 using Prometheus.Advanced;
 using tik4net;
 using tik4net.Objects;
+using tik4net.Objects.CapsMan;
 
 namespace MikrotikExporter
 {
-    class Program
+    internal static class Program
     {
-        static async System.Threading.Tasks.Task Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            var con = await tik4net.ConnectionFactory.OpenConnectionAsync(TikConnectionType.Api_v2, "192.168.0.1", "read", "");
+            var con = await ConnectionFactory.OpenConnectionAsync(TikConnectionType.ApiSsl_v2, "192.168.0.1", "read", "");
             DefaultCollectorRegistry.Instance.RegisterOnDemandCollectors(new MikrotikCollector(con));
-            var server = new Prometheus.MetricServer("127.0.0.1",1234).Start();
+            var server = new MetricServer("127.0.0.1", 1234).Start();
             Console.ReadKey();
             await server.StopAsync();
         }
     }
 
-    class MikrotikCollector : IOnDemandCollector
+    internal class MikrotikCollector : IOnDemandCollector
     {
         private readonly ITikConnection connection;
-        private Counter txBytes;
         private Counter rxBytes;
         private Gauge signal;
+        private Counter txBytes;
 
         public MikrotikCollector(ITikConnection connection)
         {
@@ -34,16 +36,17 @@ namespace MikrotikExporter
 
         public void RegisterMetrics(ICollectorRegistry registry)
         {
-             txBytes = Metrics.WithCustomRegistry(registry).CreateCounter("CAPsMAN_Clients_TxBytes", "Bytes transmitted to the CAPsMAN client", "MAC");
-             rxBytes = Metrics.WithCustomRegistry(registry).CreateCounter("CAPsMAN_Clients_RxBytes", "Bytes received from the CAPsMAN client", "MAC");
-             signal = Metrics.WithCustomRegistry(registry).CreateGauge("CAPsMAN_Clients_Signal", "CAPsMAN Client's signal", "MAC");
-
+            txBytes = Metrics.WithCustomRegistry(registry).CreateCounter("CAPsMAN_Clients_TxBytes",
+                "Bytes transmitted to the CAPsMAN client", "MAC");
+            rxBytes = Metrics.WithCustomRegistry(registry).CreateCounter("CAPsMAN_Clients_RxBytes",
+                "Bytes received from the CAPsMAN client", "MAC");
+            signal = Metrics.WithCustomRegistry(registry)
+                .CreateGauge("CAPsMAN_Clients_Signal", "CAPsMAN Client's signal", "MAC");
         }
 
         public void UpdateMetrics()
         {
-           
-            var clients = connection.LoadAll<tik4net.Objects.CapsMan.CapsManRegistrationTable>();
+            var clients = connection.LoadAll<CapsManRegistrationTable>();
             foreach (var client in clients)
             {
                 var match = Regex.Match(client.Bytes, "([0-9]+),([0-9]+)");
